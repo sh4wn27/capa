ec3bd9f2866ff0c59edf3a64d30a257491d61035
Shawn Li <huanxuan@macbook-pro-5.mynetworksettings.com>
2025-12-30 15:46:30 -0500

    activate

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ const createWindow = () => {
 app.on('ready', () => {
   createWindow()
 })
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
